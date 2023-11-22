b55184e62cf5d4ab2d4caf97a76bdb26c38720cd
Yasel Quintero <y.i.quinterolares@tudelft.nl>
2023-11-23 00:44:07 +0100

    Add test environment

--- tests/testoctp.cpp
+++ tests/testoctp.cpp
@@ -1,7 +1,35 @@
 #include <gtest/gtest.h>
+#include <iostream>
 
-class OctpTest : public testing::Test {};
+#define XSTR(x) STR(x)
+#define STR(x) #x
+
+class OctpTest : public testing::Test
+{
+public:
+    OctpTest() {}
+};
+
+class TestEnvironment : public ::testing::Environment {
+public:
+    virtual void SetUp() { 
+        std::cout << XSTR(LAMMPS_PATH) << std::endl;
+    }
+};
+
 
 TEST_F(OctpTest, DummyTest) {
     EXPECT_TRUE(true);
+}
+
+TEST_F(OctpTest, DummyTest2) {
+    EXPECT_TRUE(true);
+}
+
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  // gtest takes ownership of the TestEnvironment ptr - we don't delete it.
+  ::testing::AddGlobalTestEnvironment(new TestEnvironment);
+  return RUN_ALL_TESTS();
 }

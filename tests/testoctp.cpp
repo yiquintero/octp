0fbb91c468b1f3477e7f8558c126b4bd4b0adc12
Yasel Quintero <y.i.quinterolares@tudelft.nl>
2023-11-23 15:20:48 +0100

    Run Lammps simulation as part of tests env setup

--- tests/testoctp.cpp
+++ tests/testoctp.cpp
@@ -1,28 +1,77 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 
 #define XSTR(x) STR(x)
 #define STR(x) #x
 
-class OctpTest : public testing::Test
-{
+std::string getLAMMPSPath() {
+    std::stringstream buf;
+    buf << XSTR(LAMMPS_PATH);
+    return buf.str();
+}
+
+std::string getTestDataPath() {
+    std::stringstream buf;
+    buf << XSTR(TEST_DATA_PATH);
+    return buf.str();
+}
+
+class OctpTest : public testing::Test {
 public:
-    OctpTest() {}
+    OctpTest()
+    : testdatapath(getTestDataPath()), lammpspath(getLAMMPSPath())
+    {}
+
+    const std::string testdatapath;
+    const std::string lammpspath;
 };
 
 class TestEnvironment : public ::testing::Environment {
 public:
     virtual void SetUp() { 
-        std::cout << XSTR(LAMMPS_PATH) << std::endl;
+        // Run LAMMPS + octp to generate testing data that will be compared to gold standard
+        // 1. open LAMMPS dir
+        std::string cmnd = "cd " + getLAMMPSPath();
+        // 2. copy simulation input files
+        cmnd.append(" && cp --verbose " + getTestDataPath() + "gold-standard/simulation.in .");
+        cmnd.append(" && cp --verbose " + getTestDataPath() + "gold-standard/data.lmp .");
+        cmnd.append(" && cp --verbose " + getTestDataPath() + "gold-standard/forcefield.data .");
+        // 3. run simulation
+        cmnd.append(" && echo '...Running LAMMPS simulation...' && mpirun -n 8 lmp_mpi -in simulation.in > simoutput.txt");
+        // 4. copy output files to octp/tests/data/
+        std::vector<std::string> simulation_output_files = {
+            "diffonsag.dat",
+            "diffself.dat",
+            "log.lammps",
+            "PotenE.dat",
+            "pressure.dat",
+            "rdf.dat",
+            "tconductivity.dat",
+            "temperature.dat",
+            "TotalE.dat",
+            "viscosity.dat",
+            "simoutput.txt"
+        };
+        for (std::string file : simulation_output_files){
+            std::cout << file << std::endl;
+            cmnd.append(" && cp --verbose " + file + " " + getTestDataPath());
+        }
+        // run commands
+        std::system(cmnd.c_str());
     }
+
 };
 
 
 TEST_F(OctpTest, DummyTest) {
+    std::cout << "dumdum1 " + lammpspath << std::endl << testdatapath << std::endl;
     EXPECT_TRUE(true);
 }
 
 TEST_F(OctpTest, DummyTest2) {
+    std::cout << "dumdum2 " + lammpspath << std::endl << testdatapath << std::endl;
     EXPECT_TRUE(true);
 }
 
